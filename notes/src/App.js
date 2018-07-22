e663179cd3ea0e19f05829e41cd281373cfc811a
Ishwar Jangid <ishwarjangid@Ishwars-MacBook-Air.local>
2018-07-22 15:56:30 +0530

    Redux-Thunk

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import Notes from './components/notes';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configurationStore';
+
+const store = configureStore();
+
+// You can also pass in an initialState here
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-        < Notes />
-      </div>
-    );
-  }
+render() {
+  return (
+ <Provider store = {store}>
+    <Notes />
+  </Provider>
+  )
 }
+
+}
+
 
 export default App;
